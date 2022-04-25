0f943ef1bffa80c6d2c578c66660a2b77de005ab
kasperobig <kassivenceslas@gmail.com>
2022-04-25 13:58:08 +0300

    main

---
+++ frequency_analystics.py
@@ -0,0 +1,31 @@
+def getFrequency(text):
+    freqTab = {}
+    rightchecker = len(text) - 1
+    leftchecker = 0
+
+    # handle case
+    if rightchecker % 2 == 0:
+        mid = int(rightchecker/2)
+        if text[mid] in freqTab:
+            freqTab[text[mid]] += self.INCREMENT
+        else:
+            freqTab[text[mid]] = self.INCREMENT
+
+    for i in text:
+        if i in freqTab:
+            freqTab[i] += 1
+        else:
+            freqTab[i] = 1
+
+    print('result is: \n', freqTab)
+    printOutMsg = '\nOption: \n1) Take replace rule \n2) Exit'
+    print(printOutMsg)
+    userAction = int(input())
+    if actUser == 1:
+        encDecryptMessage()
+        return
+    if actUser == 2:
+        quit()
+    else:
+        print("invalid option selected, quiting program")
+        return
